e46915a050d63d505a32e52d48e8b503e3705deb
jsuper <tangling.life@gmail.com>
2013-03-04 23:04:13 -0500

    add template.sh

---
+++ template.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+### BEGIN INFOMATION
+# Require-Install: $build-essentials 
+# Download-Address: http://www.baidu.com
+# Provides: author
+# Description: This is used to install or uninstall author
+# Group: org.tony.demo
+### END INFORMATION
+
+case $1 in
+    install)
+
+    
+    uninstall)
+esac
+#when successfully installed, script need exit with 0 status.
+exit 0
