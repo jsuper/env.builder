8d24ba81a7e996c53dc94d9f6dceba6bb1c4ecf1
jsuper <ling.java@gmail.com>
2013-03-14 04:52:49 -0400

    commit

---
+++ java/jdk-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+### BEGIN INFO
+# Version:            1.0
+# Name:               Software_name
+# Provider:           author
+# Description:        This is used to install or uninstall author
+# Group:              org.tony.demo
+### END INFO
+
+install_file_path="$1"
+echo "$install_file_path"
