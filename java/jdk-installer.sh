3b51a740714612cbf8e2be373b364b201c2bfcc8
jsuper <ling.java@gmail.com>
2013-03-19 02:36:40 -0400

    finished jdk install

--- java/jdk-installer.sh
+++ java/jdk-installer.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 ### This script is used to install java development toolkit
-
+BASEDIR=$(dirname `readlink -f "$0"`)
 ### install from local file
 install_from_local () {
     extension_name="${1#*.}"
+    file_name="${1##*/}"
+
     case $extension_name in
 
 	tar.gz)
-            echo is tar.gz
+            mkdir -p /opt/java/jdk
+	    cp $1 /opt/java/jdk
+	    cd /opt/java/jdk
+	    pwd
+	    tar xvf $file_name
+	    target=`ls -l | grep '^d.*jdk'|awk '{print $9}'`
+	    java_home=\"`pwd`\/$target\"
+	    echo JAVA_HOME=$java_home>>$BASEDIR/../set-env.sh
+	    echo PATH=\"\$PATH:\$JAVA_HOME/bin\">>$BASEDIR/../set-env.sh
+	    echo>>$BASEDIR/../set-env.sh
+	    source /home/anylinux/.bash_profile
+	    echo "Java Development Toolkit is installed successfully"
 	    ;;
 	
 	zip)
@@ -25,7 +38,7 @@ install_from_network () {
 }
 
 if [ -f "$1" ]; then
-    install_from_local
+    install_from_local $1
 else
     install_from_network
 fi
